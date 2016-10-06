ed8153dee9ab4d883fe17c13f3cedd22400aee51
hyde <putainms@outlook.com>
2016-10-06 18:51:44 +0200

    Readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# ChatRedis
+
+ChatRedis enable you to chat on a redis server
+
+![Screenshot](chatRedis.png)
+
+### Get It Working
+1. Go in src
+2. Launch a terminal and type 
+
+```bash
+python3 mainWindow.py
+```
+3. Connect to a server
+4. Add a channel
+5. Start chating
+
+## Folders
+* src : sources of the project
+* dist : executable of the project (TODO)
+
+### Class
+* mainWindow : The main window with gtk
+* ...
+
+### Imports
+* Redis
+* GTK3
+
+### TODO
+* Some stuff
---
+++ src/ThreadMessage.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import threading
+import time
+
+
+class myThread (threading.Thread):
+    """The second thread to get the messages"""
+
+    def __init__(self, mainWindow):
+        super(myThread, self).__init__()
+        self.mainWindow = mainWindow
+        self.loop = True
+
+    def run(self):
+        print ("Starting Thread for scan messages")
+        while(self.loop):
+            time.sleep(1)
+            data = self.mainWindow.redisServer.getMessage()
+            if not data is None:
+                channel = data['channel'].decode("utf-8")
+                try:  # if you subscribe to a channel data return an int
+                    message = data['data'].decode("utf-8")
+                    self.mainWindow.updateTextview(channel, "username", message)
+                except Exception as ex:  # throw an exception if data[channel] isn't a binary string'
+                    print(ex)
+        print ("Exiting Thread for scan messages")
